a585665c4bdda3a5384c2a0e604f7d8268ba5c3e
Lukman <serlifa@gmail.com>
2022-08-01 05:33:15 +0700

    route lupa

--- routes/web.php
+++ routes/web.php
@@ -13,70 +13,20 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-})->name("index");
-
-Route::get('/peta', function () {
-    return view('peta');
-})->name('peta');
-
-Route::get('/sambutan', function () {
-    return view('sambutan');
-})->name('sambutan');
-
-Route::get('/filosofi', function () {
-    return view('filosofi');
-})->name('filosofi');
-
-Route::get('/merch', function () {
-    return view('merch');
-})->name('merch');
-
-Route::get('/gallery', function () {
-    return view('gallery');
-})->name('gallery');
-
-Route::get('/fakultas', function () {
-    return view('fakultas');
-})->name('fakultas');
-
-Route::get('/anthem', function () {
-    return view('anthem');
-})->name('anthem');
-
-Route::get('/ukm', function () {
-    return view('ukm');
-})->name('ukm');
-
-Route::get('/logo', function () {
-    return view('logo');
-})->name('logo');
-
-Route::get('/maskot', function () {
-    return view('maskot');
-})->name('maskot');
-
-Route::get('/countdown', function () {
-    return view('countdown');
-})->name('countdown');
-
-Route::get('/Tugas 1 Pra-Bakti', function () {
-    return view('tugas1pra');
-})->name('tugas1pra');
-
-Route::get('/Tugas 2 Pra-Bakti', function () {
-    return view('tugas2pra');
-})->name('tugas2pra');
-
-Route::get('/Tugas 3 Pra-Bakti', function () {
-    return view('tugas3pra');
-})->name('tugas3pra');
-
-Route::get('/Tugas 1 Pasca-Bakti', function () {
-    return view('tugas1pasca');
-})->name('tugas1pasca');
-
-Route::get('/Tugas 2 Pasca-Bakti', function () {
-    return view('tugas2pasca');
-})->name('tugas2pasca');
+Route::view('', view('welcome'))->name('index');
+Route::view('Peta', view('peta'))->name('peta');
+Route::view('Sambutan', view('sambutan'))->name('sambutan');
+Route::view('Filosofi', view('filosofi'))->name('filosofi');
+Route::view('Merch', view('merch'))->name('merch');
+Route::view('Gallery', view('gallery'))->name('gallery');
+Route::view('Fakultas', view('fakultas'))->name('fakultas');
+Route::view('Anthem', view('anthem'))->name('anthem');
+Route::view('Ukm', view('ukm'))->name('ukm');
+Route::view('Logo', view('logo'))->name('logo');
+Route::view('Maskot', view('maskot'))->name('maskot');
+Route::view('Countdown', view('countdown'))->name('countdown');
+Route::view('Tugas 1 Pra-Bakti', view('tugas1pra'))->name('tugas1pra');
+Route::view('Tugas 2 Pra-Bakti', view('tugas2pra'))->name('tugas2pra');
+Route::view('Tugas 3 Pra-Bakti', view('tugas3pra'))->name('tugas3pra');
+Route::view('Tugas 1 Pasca-Bakti', view('tugas1pasca'))->name('tugas1pasca');
+Route::view('Tugas 2 Pasca-Bakti', view('tugas2pasca'))->name('tugas2pasca');
