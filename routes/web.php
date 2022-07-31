133f4a1f2fc69ebb7280129ede08349ab7611582
Lukman <serlifa@gmail.com>
2022-08-01 05:45:45 +0700

    route lupa

--- routes/web.php
+++ routes/web.php
@@ -13,18 +13,18 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::view('', view('welcome'))->name('index');
-Route::view('Peta', view('peta'))->name('peta');
-Route::view('Sambutan', view('sambutan'))->name('sambutan');
-Route::view('Merch', view('merch'))->name('merch');
-Route::view('Gallery', view('gallery'))->name('gallery');
-Route::view('Fakultas', view('fakultas'))->name('fakultas');
-Route::view('Anthem', view('anthem'))->name('anthem');
-Route::view('Ukm', view('ukm'))->name('ukm');
-Route::view('Logo', view('logo'))->name('logo');
-Route::view('Maskot', view('maskot'))->name('maskot');
-Route::view('Tugas 1 Pra-Bakti', view('tugas1pra'))->name('tugas1pra');
-Route::view('Tugas 2 Pra-Bakti', view('tugas2pra'))->name('tugas2pra');
-Route::view('Tugas 3 Pra-Bakti', view('tugas3pra'))->name('tugas3pra');
-Route::view('Tugas 1 Pasca-Bakti', view('tugas1pasca'))->name('tugas1pasca');
-Route::view('Tugas 2 Pasca-Bakti', view('tugas2pasca'))->name('tugas2pasca');
+Route::view('','welcome')->name('index');
+Route::view('Peta','peta')->name('peta');
+Route::view('Sambutan','sambutan')->name('sambutan');
+Route::view('Merch','merch')->name('merch');
+Route::view('Gallery','gallery')->name('gallery');
+Route::view('Fakultas','fakultas')->name('fakultas');
+Route::view('Anthem','anthem')->name('anthem');
+Route::view('Ukm','ukm')->name('ukm');
+Route::view('Logo','logo')->name('logo');
+Route::view('Maskot','maskot')->name('maskot');
+Route::view('Tugas 1 Pra-Bakti','tugas1pra')->name('tugas1pra');
+Route::view('Tugas 2 Pra-Bakti','tugas2pra')->name('tugas2pra');
+Route::view('Tugas 3 Pra-Bakti','tugas3pra')->name('tugas3pra');
+Route::view('Tugas 1 Pasca-Bakti','tugas1pasca')->name('tugas1pasca');
+Route::view('Tugas 2 Pasca-Bakti','tugas2pasca')->name('tugas2pasca');
